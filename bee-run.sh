1bb7c4a7e5381f50c4c063decd0f43dd90a8100f
liuhong1happy <liuhong1.happy@163.com>
2015-11-09 13:03:38 +0800

    Update bee-run.sh

--- bee-run.sh
+++ bee-run.sh
@@ -3,7 +3,9 @@ export MGO_DATABASE=admin
 export MGO_USERNAME=mongo
 export MGO_PASSWORD=123456
 export WINAPP_DATABASE=winapp
-
+export SESSION_ON=true
+export SESSION_PROVIDER=redis
+export SESSION_SAVEPATH=192.168.1.10:6379
 
 cd $GOPATH/src/github.com/liuhong1happy/ConsoleWindowApp
 go clean -i
