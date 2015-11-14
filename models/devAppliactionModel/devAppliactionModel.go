6c5028ebee696d278fa3b908d33aa050e876b0a4
liuhong1happy <liuhong1.happy@163.com>
2015-11-14 22:57:18 +0800

    Update devAppliactionModel.go

--- models/devAppliactionModel/devAppliactionModel.go
+++ models/devAppliactionModel/devAppliactionModel.go
@@ -9,8 +9,8 @@ type{
 		ID        bson.ObjectId `bson:"_id,omitempty"`
 		AppName  string  `bson:"app_name" json:"app_name"`
 		AppContent      string    `bson:"app_content" json:"app_content"`
-	    CanUse bool  `bson:"can_use" json:"can_use"`
-	    CurrentVersionID  string  `bson:"current_version_id" json:"current_version_id"`
+		CanUse bool  `bson:"can_use" json:"can_use"`
+		CurrentVersionID  string  `bson:"current_version_id" json:"current_version_id"`
 	}
 	
 	DevApplicationVersion struct {
@@ -18,7 +18,7 @@ type{
 		AppID  string        `bson:"app_id" json:"app_id"`
 		VersionName string `bson:"version_name" json:"version_name"`
 		VersionContent string `bson:"version_content" json:"version_content"`
-	    AppSettings string  `bson:"app_settings" json:"app_settings"`
-	    CanUse bool  `bson:"can_use" json:"can_use"`
+		AppSettings string  `bson:"app_settings" json:"app_settings"`
+		CanUse bool  `bson:"can_use" json:"can_use"`
 	}
 }
